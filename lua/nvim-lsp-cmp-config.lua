93b5f3b5241713185b7b6112f4f4b318c9bcacb3
eoh <ellokri89@gmail.com>
2022-07-24 22:48:07 -0400

    add shortcuts for lspconfig

--- lua/nvim-lsp-cmp-config.lua
+++ lua/nvim-lsp-cmp-config.lua
@@ -62,15 +62,49 @@ cmp.setup.filetype('gitcommit', {
 })
 
 -- Setup lspconfig.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup({
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = on_attach
 })
 lspconfig.bashls.setup({
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = on_attach
 })
 lspconfig.gopls.setup({
 	cmd = { 'gopls', 'serve' },
@@ -84,11 +118,14 @@ lspconfig.gopls.setup({
         staticcheck = true,
       }
     },
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = on_attach
 })
 lspconfig.tsserver.setup({
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = on_attach
 })
 lspconfig.eslint.setup({
-	capabilities = capabilities
+	capabilities = capabilities,
+	on_attach = on_attach
 })
