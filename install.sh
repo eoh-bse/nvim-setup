0acb0d70528beea1da119caf96cba8a3433aaa1d
Elbert Oh <eoh@wisesystems.com>
2022-08-04 12:11:09 -0400

    add message about Jetbrains Mono in install.sh

--- install.sh
+++ install.sh
@@ -27,3 +27,5 @@ sudo npm i -g typescript typescript-language-server vscode-langservers-extracted
 
 go install golang.org/x/tools/gopls@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
+
+echo "If you want things to look even better, install Jetbrains Mono font: https://www.jetbrains.com/lp/mono/#how-to-install"
