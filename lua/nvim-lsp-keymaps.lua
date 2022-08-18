7786522d3ee85ccabb0977ca9288ed21e392621d
Elbert Oh <eoh@wisesystems.com>
2022-08-18 19:01:24 -0400

    add lsp code action keymap

--- lua/nvim-lsp-keymaps.lua
+++ lua/nvim-lsp-keymaps.lua
@@ -7,6 +7,7 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
