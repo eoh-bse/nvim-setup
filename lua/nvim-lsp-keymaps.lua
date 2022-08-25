82b9ec9c022fc4248f58d28bb2f35e56023057a5
Elbert Oh <eoh@wisesystems.com>
2022-08-25 09:29:38 -0400

    remove duplicate keymaps for lsp code action

--- lua/nvim-lsp-keymaps.lua
+++ lua/nvim-lsp-keymaps.lua
@@ -20,7 +20,6 @@ local on_attach = function(client, bufnr)
   end, bufopts)
   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
