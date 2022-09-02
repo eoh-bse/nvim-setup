c44d44d671489065f0d4e08058dd7b09631f395c
Elbert Oh <ellokri89@gmail.com>
2022-09-02 15:23:34 -0400

    add keymap for scrolling through documentation in autocompletion window (#1)
    * add keymap for scrolling through documentation in autocompletion window
    
    * convert indentations to tabs
    
    * add keymap for codelens refresh
    
    * clean up comments in cmp-config
    
    Co-authored-by: Elbert Oh <oh.applications@gmail.com>

--- lua/nvimtree-config.lua
+++ lua/nvimtree-config.lua
@@ -1,13 +1,13 @@
 return require('nvim-tree').setup({
 	disable_netrw = true,
 	hijack_netrw = true,
-    update_focused_file = {
+	update_focused_file = {
 		enable = true,
 		-- update_cwd = true
-    },
-    diagnostics = {
+	},
+	diagnostics = {
 		enable = true
-    },
+	},
 	view = {
 		side = 'left'
 	},
