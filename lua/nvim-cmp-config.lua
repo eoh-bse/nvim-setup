223ad9c6374705dbbb7fecd56f3eef043a23d5f5
Elbert Oh <ellokri89@gmail.com>
2022-09-02 15:19:23 -0400

    convert indentations to tabs

--- lua/nvim-cmp-config.lua
+++ lua/nvim-cmp-config.lua
@@ -1,64 +1,64 @@
 local cmp = require('cmp')
 
 cmp.setup({
-  completion = {
-	completeopt = 'menu,menuone,noinsert',
-  },
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
-	['<C-j>'] = cmp.mapping.scroll_docs(3),
-	['<C-k>'] = cmp.mapping.scroll_docs(-3),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'treesitter' },
-    { name = 'buffer' },
-    { name = 'vsnip' }, -- For vsnip users.
-    { name = 'path' },
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' },
-  })
+	completion = {
+		completeopt = 'menu,menuone,noinsert',
+	},
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+		vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+		-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+		-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+		-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		-- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		-- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<Tab>'] = function(fallback)
+		if cmp.visible() then
+			cmp.select_next_item()
+		else
+			fallback()
+		end
+		end,
+		['<S-Tab>'] = function(fallback)
+		if cmp.visible() then
+			cmp.select_prev_item()
+		else
+			fallback()
+		end
+		end,
+		['<C-j>'] = cmp.mapping.scroll_docs(3),
+		['<C-k>'] = cmp.mapping.scroll_docs(-3),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'treesitter' },
+		{ name = 'buffer' },
+		{ name = 'vsnip' }, -- For vsnip users.
+		{ name = 'path' },
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
+	})
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
+	sources = cmp.config.sources({
+		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = 'buffer' },
+	})
 })
--- lua/nvim-lsp-config.lua
+++ lua/nvim-lsp-config.lua
@@ -26,28 +26,28 @@ local on_attach = require('nvim-lsp-keymaps')
 local lspconfig = require('lspconfig')
 
 lspconfig.sumneko_lua.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'},
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
-    }
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {'vim'},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	}
 })
 
 lspconfig.pyright.setup({
@@ -64,20 +64,20 @@ lspconfig.gopls.setup({
 	cmd = { 'gopls', 'serve' },
 	filetypes = { 'go', 'gomod' },
 	root_dir = lspconfig.util.root_pattern('go.work', 'go.mod', '.git'),
-    settings = {
-      gopls = {
-        analyses = {
-          unusedparams = true,
-		  shadow = true,
-		  fieldalignment = true,
-		  nilness = true,
-		  unusedWrite = true,
-		  useany = true,
-		  unusedvariable = true,
-        },
-        staticcheck = true,
-      }
-    },
+	settings = {
+		gopls = {
+			analyses = {
+			unusedparams = true,
+			shadow = true,
+			fieldalignment = true,
+			nilness = true,
+			unusedWrite = true,
+			useany = true,
+			unusedvariable = true,
+			},
+			staticcheck = true,
+		}
+	},
 	init_options = {
 		usePlaceholders = true
 	},
@@ -89,18 +89,18 @@ lspconfig.gopls.setup({
 })
 
 function GoOrganizeImports(wait_ms)
-    local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-    for _, res in pairs(result or {}) do
-    	for _, r in pairs(res.result or {}) do
-    		if r.edit then
+	local params = vim.lsp.util.make_range_params()
+	params.context = {only = {"source.organizeImports"}}
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+	for _, res in pairs(result or {}) do
+		for _, r in pairs(res.result or {}) do
+			if r.edit then
 				vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
 			else
 				vim.lsp.buf.execute_command(r.command)
 			end
 		end
-    end
+	end
 end
 
 vim.cmd [[
