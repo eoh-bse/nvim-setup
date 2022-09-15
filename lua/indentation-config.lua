487877b68c1945aaa017bd141d4248b841a4e565
Elbert Oh <ellokri89@gmail.com>
2022-09-15 19:51:19 -0400

    Setup C/C++ (#4)
    * add indentation configs for each language
    
    * setup cpp

---
+++ lua/indentation-config.lua
@@ -0,0 +1,5 @@
+vim.cmd [[
+	autocmd FileType go setlocal shiftwidth=4 noexpandtab
+	autocmd FileType javascript,typescript setlocal expandtab
+	autocmd FileType cpp setlocal shiftwidth=2 expandtab
+]]
