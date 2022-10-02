902c2ec1998944939ec908faf9bca0a9dd3daed8
Elbert Oh <ellokri89@gmail.com>
2022-10-02 19:03:44 -0400

    remove error in nvim-scala-config.lua

--- lua/nvim-scala-config.lua
+++ lua/nvim-scala-config.lua
@@ -1,7 +1,5 @@
 local api = vim.api
 
-vim.opt_global.shortmess:remove("F"):append("c")
-
 local metals_config = require("metals").bare_config()
 
 -- Example of settings
@@ -25,46 +23,46 @@ metals_config.capabilities = require("cmp_nvim_lsp").update_capabilities(capabil
 local dap = require("dap")
 
 dap.configurations.scala = {
-  {
-    type = "scala",
-    request = "launch",
-    name = "RunOrTest",
-    metals = {
-      runType = "runOrTestFile",
-      --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-      env = {
-        STAGE = "local_dev"
-      },
-      envFile = ".env",
-    }
-  },
-  {
-    type = "scala",
-    request = "launch",
-    name = "Test Target",
-    metals = {
-      runType = "testTarget",
+    {
+        type = "scala",
+        request = "launch",
+        name = "RunOrTest",
+        metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+            env = {
+                STAGE = "local_dev"
+            },
+            envFile = ".env",
+        }
     },
-  },
+    {
+        type = "scala",
+        request = "launch",
+        name = "Test Target",
+        metals = {
+            runType = "testTarget",
+        },
+    },
 }
 
 local on_attach = require('nvim-lsp-keymaps')
 metals_config.on_attach = function(client, bufnr)
-  on_attach(client, bufnr)
-  require("metals").setup_dap()
+    on_attach(client, bufnr)
+    require("metals").setup_dap()
 end
 
 -- Autocmd that will actually be in charging of starting the whole thing
 local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
 api.nvim_create_autocmd("FileType", {
-  -- NOTE: You may or may not want java included here. You will need it if you
-  -- want basic Java support but it may also conflict if you are using
-  -- something like nvim-jdtls which also works on a java filetype autocmd.
-  pattern = { "scala", "sbt", "java" },
-  callback = function()
-    require("metals").initialize_or_attach(metals_config)
-  end,
-  group = nvim_metals_group,
+    -- NOTE: You may or may not want java included here. You will need it if you
+    -- want basic Java support but it may also conflict if you are using
+    -- something like nvim-jdtls which also works on a java filetype autocmd.
+    pattern = { "scala", "sbt", "java" },
+    callback = function()
+        require("metals").initialize_or_attach(metals_config)
+    end,
+    group = nvim_metals_group,
 })
 
 vim.cmd [[hi! link LspReferenceText CursorColumn]]
