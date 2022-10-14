2139cf5314851d3f2d1c120b93a5ca945c796de8
Elbert Oh <ellokri89@gmail.com>
2022-10-14 09:54:32 -0400

    fix indentation for javascript

--- lua/per-language-config.lua
+++ lua/per-language-config.lua
@@ -1,5 +1,6 @@
 vim.cmd [[
 	autocmd FileType go setlocal noexpandtab tabstop=4 shiftwidth=4
-	autocmd FileType javascript,typescript setlocal expandtab tabstop=2 shiftwidth=2
+	autocmd FileType javascript,typescript setlocal expandtab tabstop=4 shiftwidth=4
+	autocmd FileType json setlocal expandtab tabstop=2 shiftwidth=2
 	autocmd FileType cpp setlocal expandtab tabstop=2 shiftwidth=2 textwidth=80 colorcolumn=80
 ]]
