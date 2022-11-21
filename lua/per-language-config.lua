1816847476977f3e5e0059433c5c2763d816e729
Elbert Oh <ellokri89@gmail.com>
2022-11-21 09:53:38 -0500

    set up basic lsp for java

--- lua/per-language-config.lua
+++ lua/per-language-config.lua
@@ -1,5 +1,6 @@
 vim.cmd [[
 	autocmd FileType go setlocal noexpandtab tabstop=4 shiftwidth=4
+	autocmd FileType java setlocal expandtab tabstop=2 shiftwidth=2 textwidth=100 colorcolumn=100
 	autocmd FileType javascript,typescript,css,html,json setlocal expandtab tabstop=2 shiftwidth=2
 	autocmd FileType cpp setlocal expandtab tabstop=2 shiftwidth=2 textwidth=80 colorcolumn=80
 ]]
