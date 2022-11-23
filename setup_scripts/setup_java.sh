921afadc4d8fd01b014ce883f63eb5f4fa349dd7
Elbert Oh <ellokri89@gmail.com>
2022-11-23 08:56:44 -0500

    force overwriting when moving the java-language-server to $HOME/bin

--- setup_scripts/setup_java.sh
+++ setup_scripts/setup_java.sh
@@ -25,7 +25,7 @@ mkdir -p $bin_dir
 mkdir -p $dir_name
 wget -c https://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz -O $gzip_name
 tar -xzf $gzip_name -C $dir_name
-mv -n $dir_name $bin_dir
+mv -f $dir_name $bin_dir
 
 if ! grep -q $jdtls_path "$HOME/.bashrc"; then
 	echo "Adding jdtls to path..."
