f4729f5f2523458294a9bcd4c1006e2646636980
Elbert Oh <ellokri89@gmail.com>
2023-05-09 16:04:43 -0400

    attach per buffer autocmd for auto formatting

--- lua/nvim-lsp-config.lua
+++ lua/nvim-lsp-config.lua
@@ -24,14 +24,14 @@ local function create_lsp_capabilities(snippetSupport)
 	return capabilities
 end
 
-local on_attach = require('nvim-lsp-keymaps')
+local setup_on_attach = require('nvim-lsp-keymaps')
 
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 local lspconfig = require('lspconfig')
 
 lspconfig.lua_ls.setup({
 	capabilities = create_lsp_capabilities(true),
-	on_attach = on_attach,
+	on_attach = setup_on_attach(true, nil, nil),
 	settings = {
 		Lua = {
 			runtime = {
@@ -44,7 +44,7 @@ lspconfig.lua_ls.setup({
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
+				library = vim.api.nvim_get_runtime_file("lua", true),
 			},
 			-- Do not send telemetry data containing a randomized but unique identifier
 			telemetry = {
@@ -56,13 +56,28 @@ lspconfig.lua_ls.setup({
 
 lspconfig.pyright.setup({
 	capabilities = create_lsp_capabilities(true),
-	on_attach = on_attach
+	on_attach = setup_on_attach(true, nil, nil)
 })
 
 lspconfig.bashls.setup({
 	capabilities = create_lsp_capabilities(true),
-	on_attach = on_attach
+	on_attach = setup_on_attach(true, nil, nil)
 })
+
+local function go_organize_imports(wait_ms)
+	local params = vim.lsp.util.make_range_params()
+	params.context = { only = { "source.organizeImports" } }
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+	for _, res in pairs(result or {}) do
+		for _, r in pairs(res.result or {}) do
+			if r.edit then
+				vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+			else
+				vim.lsp.buf.execute_command(r.command)
+			end
+		end
+	end
+end
 
 lspconfig.gopls.setup({
 	cmd = { 'gopls', 'serve' },
@@ -89,26 +104,13 @@ lspconfig.gopls.setup({
 		}
 	},
 	capabilities = create_lsp_capabilities(false),
-	on_attach = on_attach,
+	on_attach = setup_on_attach(true, nil, function()
+		go_organize_imports(1000)
+	end),
 	flags = {
 		debounce_text_changes = 150,
 	}
 })
-
-function GoOrganizeImports(wait_ms)
-	local params = vim.lsp.util.make_range_params()
-	params.context = { only = { "source.organizeImports" } }
-	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-	for _, res in pairs(result or {}) do
-		for _, r in pairs(res.result or {}) do
-			if r.edit then
-				vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-			else
-				vim.lsp.buf.execute_command(r.command)
-			end
-		end
-	end
-end
 
 lspconfig.clangd.setup({
 	cmd = {
@@ -126,46 +128,33 @@ lspconfig.clangd.setup({
 		completeUnimported = true
 	},
 	capabilities = create_lsp_capabilities(true),
-	on_attach = on_attach
+	on_attach = setup_on_attach(true, nil, nil)
 })
 
 -- Set up nvim-jdtls for full java support
 lspconfig.jdtls.setup({
 	capabilities = create_lsp_capabilities(false),
-	on_attach = on_attach
+	on_attach = setup_on_attach(true, nil, nil)
 })
 
 lspconfig.html.setup({
 	capabilities = create_lsp_capabilities(true),
-	on_attach = on_attach
+	on_attach = setup_on_attach(true, nil, nil)
 })
 lspconfig.cssls.setup({
 	capabilities = create_lsp_capabilities(true),
-	on_attach = on_attach
+	on_attach = setup_on_attach(true, nil, nil)
 })
 lspconfig.tsserver.setup({
 	capabilities = create_lsp_capabilities(true),
-	on_attach = on_attach
+	on_attach = setup_on_attach(false, nil, nil)
 })
 lspconfig.eslint.setup({
 	capabilities = create_lsp_capabilities(true),
-	on_attach = on_attach
+	on_attach = setup_on_attach(true, 'EslintFixAll', nil)
 })
 
 lspconfig.jsonls.setup({
 	capabilities = create_lsp_capabilities(true),
-	on_attach = on_attach
+	on_attach = setup_on_attach(true, nil, nil)
 })
-
-local lsp_formatting_options = {
-	formatting_options = {
-		trimTrailingWhitespace = true,
-		insertFinalNewline = true,
-	},
-	async = true
-}
-
-vim.cmd [[
-	autocmd BufWritePre * :lua vim.lsp.buf.format(lsp_formatting_options)
-	autocmd BufWritePre *.go :lua GoOrganizeImports(1000)
-]]
