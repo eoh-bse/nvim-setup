9c7cd05da7dbffcc1edc6b7d49fa054852da02f1
Elbert Oh <ellokri89@gmail.com>
2023-05-23 09:30:09 -0400

    add scala specific settings

--- lua/per-language-config.lua
+++ lua/per-language-config.lua
@@ -1,7 +1,7 @@
 vim.cmd [[
 	autocmd FileType go setlocal noexpandtab tabstop=4 shiftwidth=4
 	autocmd FileType javascript,typescript setlocal expandtab tabstop=4 shiftwidth=4
-	autocmd FileType java setlocal expandtab tabstop=2 shiftwidth=2 textwidth=100 colorcolumn=100
+	autocmd FileType java,scala setlocal expandtab tabstop=4 shiftwidth=4 textwidth=100 colorcolumn=100
 	autocmd FileType css,html,json,terraform setlocal expandtab tabstop=2 shiftwidth=2
 	autocmd FileType c,cpp setlocal expandtab tabstop=2 shiftwidth=2 textwidth=80 colorcolumn=80
 ]]
