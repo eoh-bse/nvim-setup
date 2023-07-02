84db0dee0ec71d9f7ba5220f23b3e3185f2022d3
Elbert Oh <ellokri89@gmail.com>
2023-07-02 15:18:17 -0400

    migrate from packer to lazy

--- lua/nvimtree-config.lua
+++ lua/nvimtree-config.lua
@@ -1,38 +1,37 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+return function()
+	local keymaps_on_attach = require("nvim-tree-keymaps")
 
-local keymaps_on_attach = require("nvim-tree-keymaps")
+	require('nvim-tree').setup({
+		disable_netrw = true,
+		hijack_netrw = true,
+		update_focused_file = {
+			enable = true,
+			-- update_cwd = true
+		},
+		diagnostics = {
+			enable = true
+		},
+		view = {
+			side = 'left'
+		},
+		renderer = {
+			root_folder_label = false,
+			highlight_opened_files = 'all'
+		},
+		on_attach = keymaps_on_attach
+	})
 
-require('nvim-tree').setup({
-	disable_netrw = true,
-	hijack_netrw = true,
-	update_focused_file = {
-		enable = true,
-		-- update_cwd = true
-	},
-	diagnostics = {
-		enable = true
-	},
-	view = {
-		side = 'left'
-	},
-	renderer = {
-		root_folder_label = false,
-		highlight_opened_files = 'all'
-	},
-	on_attach = keymaps_on_attach
-})
+	local function open_nvim_tree(data)
+		local is_directory = vim.fn.isdirectory(data.file) == 1
 
-local function open_nvim_tree(data)
-	local is_directory = vim.fn.isdirectory(data.file) == 1
+		if not is_directory then
+			return
+		end
 
-	if not is_directory then
-		return
+		vim.cmd.cd(data.file)
+
+		require("nvim-tree.api").tree.open()
 	end
 
-	vim.cmd.cd(data.file)
-
-	require("nvim-tree.api").tree.open()
+	vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
--- lua/nvimtreesitter-config.lua
+++ lua/nvimtreesitter-config.lua
@@ -1,33 +1,35 @@
-require('nvim-treesitter.configs').setup({
-	ensure_installed = {
-		'go',
-		'gomod',
-		'gowork',
-		'java',
-		'c_sharp',
-		'c',
-		'cpp',
-		'html',
-		'css',
-		'scss',
-		'javascript',
-		'typescript',
-		'tsx',
-		'json',
-		'bash',
-		'dockerfile',
-		'lua',
-		'regex',
-		'sql',
-		'proto',
-		"yaml",
-	},
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true
-	}
-})
+return function()
+	require('nvim-treesitter.configs').setup({
+		ensure_installed = {
+			'go',
+			'gomod',
+			'gowork',
+			'java',
+			'c_sharp',
+			'c',
+			'cpp',
+			'html',
+			'css',
+			'scss',
+			'javascript',
+			'typescript',
+			'tsx',
+			'json',
+			'bash',
+			'dockerfile',
+			'lua',
+			'regex',
+			'sql',
+			'proto',
+			"yaml",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+		indent = {
+			enable = true
+		}
+	})
+end
