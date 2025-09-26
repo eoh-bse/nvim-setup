ae619436ae49dd4f62af03e795397916a335f2eb
Elbert Oh <ellokri89@gmail.com>
2025-09-26 14:14:30 -0400

    bugfix: fix go import auto organization on save

--- lua/nvim-lsp-config.lua
+++ lua/nvim-lsp-config.lua
@@ -98,15 +98,6 @@ return function()
 	})
 	vim.lsp.enable("bashls")
 
-	local function go_organize_imports(wait_ms)
-		local params = vim.lsp.util.make_range_params(0, "utf-8")
-		params.context = { only = { "source.organizeImports" } }
-		vim.lsp.buf_request_sync(0, "workspace/executeCommand", {
-			command = "gopls.organizeImports",
-			arguments = { vim.api.nvim_buf_get_name(0) },
-		}, wait_ms)
-	end
-
 	vim.lsp.config("gopls", {
 		cmd = { "gopls" },
 		filetypes = { "go", "gomod", "gowork", "gotmpl" },
@@ -131,12 +122,34 @@ return function()
 			}
 		},
 		capabilities = capabilities_without_snippet,
-		on_attach = setup_on_attach(true, nil, function() go_organize_imports(1000) end),
+		on_attach = setup_on_attach(true, nil, nil),
 		flags = {
 			debounce_text_changes = 150
 		}
 	})
 	vim.lsp.enable("gopls")
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.go",
+		callback = function()
+			local params = vim.lsp.util.make_range_params()
+			params.context = { only = { "source.organizeImports" } }
+			-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+			-- machine and codebase, you may want longer. Add an additional
+			-- argument after params if you find that you have to write the file
+			-- twice for changes to be saved.
+			-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+			local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+			for cid, res in pairs(result or {}) do
+				for _, r in pairs(res.result or {}) do
+					if r.edit then
+						local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+						vim.lsp.util.apply_workspace_edit(r.edit, enc)
+					end
+				end
+			end
+			vim.lsp.buf.format({ async = false })
+		end
+	})
 
 	vim.lsp.config("clangd", {
 		cmd = {
